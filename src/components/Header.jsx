de73ec4ffe2beba0299b0af8e1676b4458394e8f
piv2003 <piv2003@gmail.com>
2021-03-26 18:10:10 +0200

    Добавление репозитория My-app

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from  'react'
+
+const Nav = () => {
+return <header className="header">
+    <img src='img/header.jpg'  alt='logo' />
+</header>
+
+}
+
+export default Nav;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from  'react'
+
+
+const Profile = () => {
+return  <aside className="content">
+    <div>
+        <img src='https://kuku.travel/wp-content/uploads/2017/08/Plage-de-Palombaggia.jpg' alt='beach'/>
+    </div>
+    <div>
+        <img src='img/images.png' alt='logo'/>         </div>
+    <div>
+        My posts
+        <div>
+            New post
+        </div>
+        <div>
+            <div>
+                post1
+            </div>
+            <div>
+                post 2
+            </div>
+
+
+
+        </div>
+    </div>
+</aside>
+
+}
+
+export default Profile;
