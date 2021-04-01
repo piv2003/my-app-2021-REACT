f117be9a9dc61982948f1bdbd33a2dbbba5c29ba
piv2003 <piv2003@gmail.com>
2021-04-01 11:13:44 +0300

    19. Уроки React JS (route, browser-router, маршрутизация)
    У меня где-то потерялся "контент" страницы Profile - будем искать....

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,8 +5,8 @@ import s from './Navbar.module.css';
 const Navbar = () => {
 return  <nav className={s.nav}>
     <ul className="nav-list">
-        <li className={s.item}><a href='#'>Profile</a></li>
-        <li className={s.item}><a href='#'>Messages</a></li>
+        <li className={s.item}><a href='/Profile'>Profile</a></li>
+        <li className={`${s.item} ${s.active}`}><a href='/Dialogs'>Messages</a></li>
         <li className={s.item}><a href='#'>News</a></li>
         <li className={s.item}><a href='#'>Music</a></li>
         <li className={s.item}><a href='#'>Settings</a></li>
