86a548ebfd8900d33e96044748f92caaaba451f5
piv2003 <piv2003@gmail.com>
2021-04-03 23:08:11 +0300

    20. Уроки React JS (NavLink)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,26 @@
 import React from  'react';
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 
 const Navbar = () => {
 return  <nav className={s.nav}>
     <ul className="nav-list">
-        <li className={s.item}><a href='/profile'>Profile</a></li>
-        <li className={`${s.item} ${s.active}`}><a href='/dialogs'>Messages</a></li>
-        <li className={s.item}><a href='/news'>News</a></li>
-        <li className={s.item}><a href='/music'>Music</a></li>
-        <li className={s.item}><a href='/settings'>Settings</a></li>
+        <li className={s.item}>
+            <NavLink to='/profile'>Profile</NavLink>
+        </li>
+        <li className={`${s.item} ${s.active}`}>
+            <NavLink to='/dialogs'>Messages</NavLink>
+        </li>
+        <li className={s.item}>
+            <NavLink to='/news'>News</NavLink>
+        </li>
+        <li className={s.item}>
+            <NavLink to='/music'>Music</NavLink>
+        </li>
+        <li className={s.item}>
+            <NavLink to='/settings'>Settings</NavLink>
+        </li>
     </ul>
 </nav>
 
