fa3e1c513a87f6171d56e83372895aed33639efe
piv2003 <piv2003@gmail.com>
2021-04-21 22:56:20 +0300

    26. Уроки React JS (структура проекта, ещё раз про ui - bll)

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,27 +1,26 @@
-import React from  'react';
+import React from 'react';
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
-    let posts= [
-        {id: 1, message: 'Hi, how are you?',likesCount:12},
-        {id: 2, message: 'It is my first post.',likesCount:11},
+    let posts = [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It is my first post.', likesCount: 11},
     ]
-let postsElements =
-    posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
+    let postsElements =
+        posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
-return (
-    <div className={s.postsBlock}>
-        <h3>My posts</h3>
-        <div className={s.content}>
-            <textarea className={s.size_textarea}> </textarea>
-            <button className={s.color_button}> Add post</button>
-        </div>
-
-        <div className={s.posts}>
-            {postsElements}
-        </div>
-    </div>)
+    return (
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
+            <div className={s.content}>
+                <textarea className={s.size_textarea}> </textarea>
+                <button className={s.color_button}> Add post</button>
+            </div>
+            <div className={s.posts}>
+                {postsElements}
+            </div>
+        </div>)
 }
 
 export default MyPosts;
